15ac5214edd43373b0915aa812e77ceecb819fce
Soma Shekar <Somashekar528234@gmail.com>
2025-07-09 21:06:29 +0530

    "Some minor changes"

--- configs/AImodel.js
+++ configs/AImodel.js
@@ -7,9 +7,8 @@ const {
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-exp",
-});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 
 const generationConfig = {
   temperature: 1,
