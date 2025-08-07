fa8c4205ac733f5c1b4863b36a4e8b17baf68ee0
Soma Shekar <somashekar2025@gmail.com>
2025-08-07 17:39:13 +0530

    fix: add alias config for @ to fix Render build

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
+  typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(process.cwd());
+    return config;
   },
 };
 
